25ab8829a66ccab0a285ff52805cb4cf9c13152f
Andrii Yelnikov <whiteyelnikov@gmail.com>
2024-01-24 23:00:45 +0200

    24 january database changed

--- utils/database.js
+++ utils/database.js
@@ -13,9 +13,9 @@ import { Sequelize } from 'sequelize';
 //   logging: false,
 // });
 
-export const sequelize = new Sequelize('database_24_10', 'database_24_10_user', 'OwfCX0mNX9Gl6bwkIBPPjgXNjLOFoA7c', {
-  // host: 'dpg-cks1f9mnfb1c73eo9du0-a.oregon-postgres.render.com',
-  host: 'dpg-cks1f9mnfb1c73eo9du0-a',
+export const sequelize = new Sequelize('database_24_01', 'database_24_01_user', '7jMDla7q6hWKQP3CrIb4aA5UBFQOgPgB', {
+  host: 'dpg-cmone5un7f5s73d8ummg-a.oregon-postgres.render.com',
+  // host: 'dpg-cmone5un7f5s73d8ummg-a',
   dialectOptions: {
     ssl: {
       require: true,
